c85a3ba5a312b9c25b407cf356c0e8c847ee139d
littlegblumer <143110034+littlegblumer@users.noreply.github.com>
2023-11-16 10:35:14 -0600

    Initial commit

---
+++ app/src/main/java/com/blumer/msu/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,121 @@
+package com.blumer.msu.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
+import com.blumer.msu.criminalintent.databinding.FragmentCrimeDetailBinding
+import kotlinx.coroutines.launch
+import java.util.Date
+import java.util.UUID
+
+private const val TAG = "CrimeDetailFragment"
+
+class CrimeDetailFragment : Fragment() {
+
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    private val args: CrimeDetailFragmentArgs by navArgs()
+
+    private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+        CrimeDetailViewModelFactory(args.crimeId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.crimeTitle.text.toString().isEmpty()) {Toast.makeText(
+                    requireContext(),
+                    "Please provide a title for the crime before navigating back.",
+                    Toast.LENGTH_SHORT
+                ).show()
+//                    binding.crimeTitle.setHint(R.string.crime_title_needed)
+                } else {
+                    isEnabled = false
+                    activity?.onBackPressed()
+                }
+            }
+        })
+
+        val crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+
+        Log.d(TAG, "The crime ID is: ${args.crimeId}")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding =
+            FragmentCrimeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            binding.crimeTitle.doOnTextChanged { text, _, _, _ ->
+                lifecycleScope.launch {
+                    crimeDetailViewModel.updateCrime { oldCrime ->
+                        oldCrime.copy(title = text.toString())
+                    }
+                }
+            }
+
+            crimeDate.apply {
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                lifecycleScope.launch {
+                    crimeDetailViewModel.updateCrime { oldCrime ->
+                        oldCrime.copy(isSolved = isChecked)
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                crimeDetailViewModel.crime.collect { crime ->
+                    crime?.let { updateUi(it) }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun updateUi(crime: Crime) {
+        binding.apply {
+            if (crimeTitle.text.toString() != crime.title) {
+                crimeTitle.setText(crime.title)
+            }
+            crimeDate.text = crime.date.toString()
+            crimeSolved.isChecked = crime.isSolved
+        }
+    }
+}
